6ab5622be2ba1569aef9c06711106d2204548ed2
Brian Takita <brian.takita@gmail.com>
2021-01-08 00:39:14 -0500

    update dependencies

--- signUp.d.ts
+++ signUp.d.ts
@@ -1,3 +1,3 @@
-/// <reference types=".pnpm/amazon-cognito-identity-js@4.5.6/node_modules/amazon-cognito-identity-js" />
+/// <reference types=".pnpm/amazon-cognito-identity-js@4.5.7/node_modules/amazon-cognito-identity-js" />
 import type { CognitoUserAttribute } from 'amazon-cognito-identity-js';
 export declare function signUp(username: string, password: string, attributes_a1: CognitoUserAttribute[], validation_data: CognitoUserAttribute[]): Promise<import("amazon-cognito-identity-js").ISignUpResult | undefined>;
